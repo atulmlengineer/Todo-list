51775a0c323853c0d9e98abe747bfd3f621ab940
Atul Shukla <atul.mlengineer@gmail.com>
2021-09-22 00:48:53 +0530

    Updates

---
+++ precache-manifest.ac90b22333f3ba469fa349602c024d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a33d4e2878effbbc88266b15c5d479",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "357314157837a19bc6c6",
+    "url": "/Todo-app/static/css/main.a7e224c4.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/Todo-app/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "357314157837a19bc6c6",
+    "url": "/Todo-app/static/js/main.03a5e425.chunk.js"
+  },
+  {
+    "revision": "46683e8a38d81ef81dfe",
+    "url": "/Todo-app/static/js/runtime~main.341465e3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Todo-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
